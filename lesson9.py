5cb49ee3f184e38534bddab6ff9e1a903d82e2d9
pirateorno <sasaforostovskij@gmail.com>
2022-05-25 16:30:12 +0300

    logging

---
+++ lesson9.py
@@ -0,0 +1,13 @@
+# import logging
+#
+# logging.basicConfig(level=logging.DEBUG, filename="logs.log",filemode="w",format="We have next log: %(asctime)s%(levelname)s - %(message)s")
+#
+#
+# try:
+#     print(5\0)
+# except:
+#     logging.debug("debug")
+#     logging.info("info")
+#     logging.warning("warning")
+#     logging.error("error")
+#     logging.critical("critical")
