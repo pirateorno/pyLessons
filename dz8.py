84e6c9bbd534831a92f30fa5631fc01543e7af09
pirateorno <sasaforostovskij@gmail.com>
2022-05-25 17:53:06 +0300

    DZ

--- dz8.py
+++ dz8.py
@@ -1,5 +1,16 @@
+import random
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename="logs.log",filemode="w",format="We have next log: %(asctime)s%(levelname)s - %(message)s")
 r = range(0, 10)
 it = iter(r)
 
+
 l = [1+5*(next(it)) for i in r]
+it = iter(l)
 print(l)
+for i in l:
+    try:
+        print( l[next(it)]/random.randint(-5, 5))
+    except:
+        logging.error("Division Zero")
