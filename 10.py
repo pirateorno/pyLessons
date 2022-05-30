31479b87555da25d9b0b8969c80ae383862417c2
pirateorno <sasaforostovskij@gmail.com>
2022-05-30 15:58:05 +0300

    lesson 10 import requests response.post

--- 10.py
+++ 10.py
@@ -1,4 +1,6 @@
 import requests
-response = requests.get("https://httpbin.org/get")
-print(response.content)
-print(f"datatype - {type(response.content)}")
+# response = requests.get("https://httpbin.org/get")
+# print(response.text)
+# print(f"datatype - {type(response.text)}")
+response = requests.post("https://httpbin.org/post", data="Test data", headers={"h1":"Test title"})
+print(response.text)
