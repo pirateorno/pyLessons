3f452084dd85ce3cf51464348a22b3dce2be4221
pirateorno <sasaforostovskij@gmail.com>
2022-05-30 16:24:16 +0300

    start code parser

--- 10.py
+++ 10.py
@@ -2,5 +2,17 @@ import requests
 # response = requests.get("https://httpbin.org/get")
 # print(response.text)
 # print(f"datatype - {type(response.text)}")
-response = requests.post("https://httpbin.org/post", data="Test data", headers={"h1":"Test title"})
-print(response.text)
+# response = requests.post("https://httpbin.org/post", data="Test data", headers={"h1":"Test title"})
+# print(response.text)
+
+res_parse_list = []
+response = requests.get("https://coinmarketcap.com/")
+response_text = response.text
+response_parse = response_text.split("<span>")
+for parse_elem_1 in response_parse:
+    if parse_elem_1.startswith("$"):
+        for parse_elem_2 in parse_elem_1.split("</span>"):
+            if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
+                res_parse_list.append(parse_elem_2)
+bitcoin_exchange_rate = res_parse_list[0]
+print(bitcoin_exchange_rate)
